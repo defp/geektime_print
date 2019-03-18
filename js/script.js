67e2583bf2217f624a6624a09f05f69d038bd859
Dashuang Li <ldshuang@gmail.com>
2019-03-18 15:31:43 +0800

    first version

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function () {
+  id = window.location.pathname.split("/")[3]
+ 
+  $.ajax({
+    url: "https://time.geekbang.org/serv/v1/article",
+    method: "POST",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({ id: id, include_neighbors: true}),
+    success: function (rsp) {
+     if(rsp.data){
+       article_content = rsp.data.article_content
+       $("#app").replaceWith(article_content)
+     } else {
+       console.log("api error ", rsp.error.msg)
+     }
+    }
+  })
+})();
