df878fe4ed21163f32eaad9941b4af7873dad6f4
Dashuang Li <ldshuang@gmail.com>
2019-03-18 17:41:14 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 (function () {
   id = window.location.pathname.split("/")[3]
- 
-  $.ajax({
-    url: "https://time.geekbang.org/serv/v1/article",
-    method: "POST",
-    contentType: "application/json",
-    dataType: "json",
-    data: JSON.stringify({ id: id, include_neighbors: true}),
-    success: function (rsp) {
-     if(rsp.data){
-       article_content = rsp.data.article_content
-       $("#app").replaceWith(article_content)
-     } else {
-       console.log("api error ", rsp.error.msg)
-     }
-    }
+  $("body").on("dblclick", "h1", function () {
+    $.ajax({
+      url: "https://time.geekbang.org/serv/v1/article",
+      method: "POST",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({ id: id, include_neighbors: true }),
+      success: function (rsp) {
+        if (rsp.data) {
+          article_content = rsp.data.article_content
+          $("#app").replaceWith(article_content)
+        } else {
+          console.log("api error ", rsp.error.msg)
+        }
+      }
+    })
   })
 })();
