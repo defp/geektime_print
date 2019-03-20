86a4084c0bcfd76dcb356c0e0cb8a38d80158750
Dashuang Li <ldshuang@gmail.com>
2019-03-20 21:36:18 +0800

    Update html css

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
 (function () {
-  id = window.location.pathname.split("/")[3]
 
   $("body").on("dblclick", "h1", function () {
+    var id = window.location.pathname.split("/")[3]
+
     $.ajax({
       url: "https://time.geekbang.org/serv/v1/article",
       method: "POST",
       contentType: "application/json",
       dataType: "json",
       data: JSON.stringify({ id: id, include_neighbors: true }),
+
       success: function (rsp) {
         if (rsp.data) {
-          article_content = rsp.data.article_content
-          title = "<h1 id='title'>" + rsp.data.article_title + "</h1>"
-          $("#app").replaceWith(title + article_content)
+          var html = `
+            <!DOCTYPE html>
+            <html>
+              <head>
+              <meta charset="utf-8">
+              <title>${rsp.data.article_title}</title>
+              </head>
+              <body>
+                <div id='print-content'>
+                  <h1 id='title'>
+                    ${ rsp.data.article_title}
+                  </h1>
+                  ${rsp.data.article_content}
+                </div>
+              </body>
+            </html>
+            `
 
-          $('img').css({ "width": "1000px" })
+          $("html").html(html)
         } else {
           console.log("api error ", rsp.error.msg)
         }
       }
     })
-
-    $('body').css({ "width": "1000px", "margin": "auto", "margin-top": "30px"})
   })
 })();
